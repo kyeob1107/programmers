eb632bf2fe1963cd8a47cbfdd5bac363a56d3441
kyeob1107 <154004299+kyeob1107@users.noreply.github.com>
2023-12-26 12:55:23 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 1433.68 ms, Memory: 11 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -0,0 +1,13 @@
+def solution(s):
+    answer = []
+    #print(s[:0])
+    for i in range(len(s)):
+        if s[i] in s[:i]:
+            for j in range(len(s[:i])):
+                if s[i]==s[:i][::-1][j]:
+                    answer.append(j+1)
+                    break #가장 가까운 같은 글자 찾았으면 반복하면서 확인그만
+                          #추가 확인하면 여러번 차이값을 답에 추가하게 됨
+        else:
+            answer.append(-1)
+    return answer
