dc22a02d4b4815e241280d927a6cfef589230438
kyeob1107 <154004299+kyeob1107@users.noreply.github.com>
2023-12-26 11:42:54 +0900

    [level 2] Title: 카테고리 별 상품 개수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131529. 카테고리 별 상품 개수 구하기/카테고리 별 상품 개수 구하기.sql
+++ 프로그래머스/2/131529. 카테고리 별 상품 개수 구하기/카테고리 별 상품 개수 구하기.sql
@@ -4,3 +4,10 @@ SELECT  substr(PRODUCT_CODE,1,2) CATEGORY,
 from PRODUCT
 group by 1
 order by 1
+
+/*SELECT  PRODUCT_CODE,
+        distinct(substr(PRODUCT_CODE,1,2)) CATEGORY#,
+        #count(1) PRODUCTS
+from PRODUCT
+order by 1*/
+#아래코드 안되는데 현재 왜 안되는지 모르겠어서 나중에 다시 확인
