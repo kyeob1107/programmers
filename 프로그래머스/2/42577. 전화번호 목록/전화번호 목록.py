e01bf03801d11384a34fc9c9993dc5a5354a1005
kyeob1107 <154004299+kyeob1107@users.noreply.github.com>
2024-01-09 16:50:53 +0900

    [level 2] Title: 전화번호 목록, Time: 79.92 ms, Memory: 28 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,6 +1,14 @@
-def solution(phone_book):
+def solution(phoneBook):
+    phoneBook = sorted(phoneBook)
+
+    for p1, p2 in zip(phoneBook, phoneBook[1:]):
+        if p2.startswith(p1):
+            return False
+    return True
+
+'''def solution(phone_book):
     phone_book.sort()
     for i in range(len(phone_book)-1):
         if phone_book[i]==phone_book[i+1][:len(phone_book[i])]:
             return False
-    return True
+    return True'''
