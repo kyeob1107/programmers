7905a423af5db4bdea13b4236bf7d8bd28bfdad1
곽승엽 <154004299+kyeob1107@users.noreply.github.com>
2024-06-20 11:49:30 +0900

    [Silver III] Title: 터렛, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1002. 터렛/터렛.py
+++ 백준/Silver/1002. 터렛/터렛.py
@@ -5,26 +5,23 @@ def d(x1, y1, x2, y2):
     if result%1 == 0:
         result = int(result)
     return result
-
-tc = sys.stdin.read()
-# tc = open("input.txt", "rt").read()
-lines = tc.split("\n")
-T = lines[0]
-for line in lines[1:]:
-    if line.strip():  # 공백 라인 무시
-        x1, y1, r1, x2, y2, r2 = map(int, line.split())
-        # print(x1, y1, r1, x2, y2, r2)
-        if (x1, y1) == (x2, y2):
-            if r1 == r2:
-                print(-1)
-            else:
-                print(0)
-        
+input = sys.stdin.readline
+# input = open("input.txt", "rt").readline
+T = int(input())
+for t in range(T):
+    x1, y1, r1, x2, y2, r2 = map(int, input().split())
+    # print(x1, y1, r1, x2, y2, r2)
+    if (x1, y1) == (x2, y2):
+        if r1 == r2:
+            print(-1)
         else:
-            distance = d(x1, y1, x2, y2)
-            if distance < r1 + r2 and distance > abs(r1 - r2) :
-                print(2)
-            elif distance == r1 + r2 or distance == abs(r1 - r2):
-                print(1)
-            else:
-                print(0)
+            print(0)
+    
+    else:
+        distance = d(x1, y1, x2, y2)
+        if distance < r1 + r2 and distance > abs(r1 - r2) :
+            print(2)
+        elif distance == r1 + r2 or distance == abs(r1 - r2):
+            print(1)
+        else:
+            print(0)
