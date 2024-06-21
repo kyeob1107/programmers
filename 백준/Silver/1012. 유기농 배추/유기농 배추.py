64fba5f37c62822c0f02d9e76f3e783a854f7d14
곽승엽 <154004299+kyeob1107@users.noreply.github.com>
2024-06-21 15:36:01 +0900

    [Silver II] Title: 유기농 배추, Time: 68 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -2,36 +2,37 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-def bfs(i, j):
+def bfs(start_x, start_y):
     move = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-    queue = deque([(j, i)])
+    queue = deque([(start_x, start_y)])
+    positions.remove((start_x, start_y))
+    
     while queue:
         x, y = queue.popleft()
-        if (x, y) in visited:
-            continue
-        visited.add((x, y))
         for dx, dy in move:
-            nx = x + dx
-            ny = y + dy
-            if (nx, ny) in positions and (nx, ny) not in visited:
+            nx, ny = x + dx, y + dy
+            if (nx, ny) in positions:
+                positions.remove((nx, ny))
                 queue.append((nx, ny))
 
-T = int(input())
-ans = []
-for _ in range(T):
-    M, N, K = map(int, input().split())
-    visited = set()
-    positions = set()
-    cnt = 0
-    for _ in range(K):
-        X, Y = map(int, input().split())
-        positions.add((X, Y))
-    
-    for x, y in positions:
-        if (x, y) not in visited:
-            bfs(y, x)
-            cnt += 1
-    ans.append(cnt)
+T = int(input().strip())
+results = []
 
-for a in ans:
-    print(a)
+for _ in range(T):
+    M, N, K = map(int, input().strip().split())
+    positions = set()
+    
+    for _ in range(K):
+        X, Y = map(int, input().strip().split())
+        positions.add((X, Y))
+
+    cnt = 0
+    while positions:  # positions이 비어질 때까지 반복
+        px, py = next(iter(positions))  # positions의 임의의 요소 하나 가져오기
+        bfs(px, py)
+        cnt += 1
+    
+    results.append(cnt)
+
+for result in results:
+    print(result)
