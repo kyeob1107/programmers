019cfa409e1f6053157099d508fe50f66cf934d3
곽승엽 <154004299+kyeob1107@users.noreply.github.com>
2024-06-21 11:24:38 +0900

    [Silver III] Title: 어린 왕자, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1004. 어린 왕자/어린 왕자.py
@@ -0,0 +1,24 @@
+import sys
+
+def cnt_entry_exit(x1, y1, x2, y2, n):
+    cnt = 0
+    for _ in range(n):
+        cx, cy, r = map(int, input().split())
+        
+        if (x1 - cx)**2 + (y1 -cy)**2 < r**2 and (x2 - cx)**2 + (y2 -cy)**2 > r**2:
+            cnt += 1
+        elif (x1 - cx)**2 + (y1 -cy)**2 > r**2 and (x2 - cx)**2 + (y2 -cy)**2 < r**2:
+            cnt += 1
+        
+    return cnt
+
+input = sys.stdin.readline
+T = int(input())
+results = []
+for _ in range(T):
+    x1, y1, x2, y2 = map(int, input().split())
+    N = int(input())
+    results.append(cnt_entry_exit(x1, y1, x2, y2, N))
+
+for result in results:
+    print(result)
