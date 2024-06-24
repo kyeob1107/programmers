baef18feb3035df9a3ea89a92d1dc21c7a8b7ff7
곽승엽 <154004299+kyeob1107@users.noreply.github.com>
2024-06-24 14:58:38 +0900

    [Silver IV] Title: 보물, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.py
+++ 백준/Silver/1026. 보물/보물.py
@@ -1,8 +1,7 @@
 import sys
 input = sys.stdin.readline
-
 N = int(input())
 A = sorted(map(int, input().split()), reverse = True)
 B = sorted(map(int, input().split()))
 
-print(sum([x*y for x, y in zip(A, B)]))
+print(sum(x*y for x, y in zip(A, B)))
