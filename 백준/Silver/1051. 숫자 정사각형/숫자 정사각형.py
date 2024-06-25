88467a253a37391532800a2ac1c9b81f22d56141
곽승엽 <154004299+kyeob1107@users.noreply.github.com>
2024-06-25 13:13:15 +0900

    [Silver III] Title: 숫자 정사각형, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.py
+++ 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.py
@@ -1,24 +1,14 @@
 import sys
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
-rectangle = [[] for _ in range(10)]
-for i in range(N):
-    row_temp = [int(n) for n in input().strip()]
-    for j, val in enumerate(row_temp):
-        rectangle[val%10].append((i, j))
+def square(N, M):
+    for r  in range(min(N, M), 0, -1):
+        for i in range(N - r):
+            for j in range(M - r):
+                if rectangle[i][j] == rectangle[i][j + r] == rectangle[i + r][j] == rectangle[i + r][j + r]:
+                    return (r + 1) ** 2
+    return 1
 
-ans = 1
-max_len = min(N, M)
-for positions in rectangle:
-    if len(positions) < 4:
-        continue
-    
-    for pos in positions:
-        row, col = pos
-        for r in range(max_len, 0, -1):
-            if (row + r, col) in positions:
-                if (row, col + r) in positions and (row + r, col + r) in positions:
-                    ans = max(ans, r + 1)
-                    break
-print(ans**2)
+N, M = map(int, input().split())
+rectangle = [[int(n) for n in input().strip()] for _ in range(N)]
+print(square(N, M))
