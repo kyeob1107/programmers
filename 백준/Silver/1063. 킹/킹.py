8d23f85023aeb40124ae1679e052adbef3407e5b
곽승엽 <154004299+kyeob1107@users.noreply.github.com>
2024-06-26 15:30:34 +0900

    [Silver III] Title: 킹, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1063. 킹/킹.py
+++ 백준/Silver/1063. 킹/킹.py
@@ -1,50 +1,22 @@
+# 1등 코드 확인용
 import sys
 input = sys.stdin.readline
+c = {chr(i+64):i for i in range(1,9)}
+c_r = {i:chr(i+64) for i in range(1,9)}
+dir = {"R": (0,1),"L": (0,-1),"B": (-1,0),"T": (1,0),
+       "RT": (1,1),"LT": (1,-1),"RB": (-1,1),"LB": (-1,-1)}
 
-def move(pos_king, pos_stone, string):
-    # print("디버깅: ", pos_king, pos_stone, string, end=':::')
-    # 숫자로 변환 부분
-    col = col_encoding.index(pos_king[0]) + 1
-    row = int(pos_king[1])
-    col_s = col_encoding.index(pos_stone[0]) + 1
-    row_s = int(pos_stone[1])
-    
-    # 이동 해석 및 반영 부분
-    mc, mr = 0, 0
-    if 'R' in string:
-        mc += 1
-    if 'L' in string:
-        mc -= 1
-    if 'T' in string:
-        mr += 1
-    if 'B' in string:
-        mr -= 1
-    
-    col += mc
-    row += mr
-    
-    # print(col, row, col_s, row_s)
-    if row > 8 or row < 1  or col > 8 or col < 1:
-        # print("응 아니야 돌아가:", col, row)
-        return pos_king, pos_stone
-    
-    if col == col_s and row == row_s:
-        col_s += mc
-        row_s += mr
-        if row_s > 8 or row_s < 1  or col_s > 8 or col_s < 1:
-            # print("응 아니야 돌아가2:", col_s, row_s)
-            return pos_king, pos_stone
-    
-    # 문자로 변환 부분
-    return col_encoding[col - 1] + str(row), col_encoding[col_s - 1] + str(row_s)
-
-king, stone, N = input().split()
-N = int(N)
-col_encoding = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
-# 벽 만들어서 -1로 할까하다가 효율이 없을 것 같아서 일단 보류
-
-for _ in range(N):
-    king, stone = move(king, stone, input().strip())
-    # print("디버깅2: ", king, stone)
-
-print(king, stone, sep='\n')
+k,s,n = input().split()
+kx, ky = int(k[1]),c[k[0]]
+sx, sy = int(s[1]),c[s[0]]
+for _ in range(int(n)):
+    i,j = dir[input().strip()]
+    nkx, nky = kx + i, ky + j
+    if nkx == sx and nky == sy:
+        nsx, nsy = sx + i, sy + j
+        if 0 < nsx <= 8 and 0 < nsy <= 8:
+            kx, ky, sx, sy = nkx, nky, nsx, nsy
+    elif 0 < nkx <= 8 and 0 < nky <= 8:
+        kx, ky = nkx, nky
+print("".join(map(str,[c_r[ky],kx])))
+print("".join(map(str,[c_r[sy],sx])))
