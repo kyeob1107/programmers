eac55dcd550188805a9f7d583449f0238ac67bec
곽승엽 <154004299+kyeob1107@users.noreply.github.com>
2024-06-30 16:36:02 +0900

    [Silver I] Title: 행렬, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1080. 행렬/행렬.py
+++ 백준/Silver/1080. 행렬/행렬.py
@@ -2,36 +2,32 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-matrix_a = []
-matrix_b = []
-check = [[0] * M for _ in range(N)]
+matrix_fst = []
+check = []
 for _ in range(N):
-    matrix_a.append([int(i) for i in input().strip()])
-for _ in range(N):
-    matrix_b.append([int(i) for i in input().strip()])
-
+    matrix_fst.append([int(i) for i in input().strip()])
 for y in range(N):
-    for x in range(M):
-        if matrix_a[y][x] != matrix_b[y][x]:
-            check[y][x] = 1
+    check.append([matrix_fst[y][x] != int(i) for x, i in enumerate(input().strip())])
+    
 pos_range = [(-1, -1), (-1, 0), (-1, 1), 
              (0, -1), (0, 0), (0, 1),
              (1, -1), (1, 0), (1, 1)] # y, x
 cnt = 0
+
 if N < 3 and M < 3:
-    if any(any(j==1 for j in check[i]) for i in range(N)):
+    if any(any(j for j in check[i]) for i in range(N)):
         print(-1)
     
     else:
         print(0)
+        
 else:
     for y in range(1, N - 1):
         for x in range(1, M - 1):
-            if check[y - 1][x - 1] == 1:
+            if check[y - 1][x - 1]:
                 cnt += 1
                 for dy, dx in pos_range:
-                    check[y + dy][x + dx] = (check[y + dy][x + dx] + 1)%2
-    
+                    check[y + dy][x + dx] = not check[y + dy][x + dx]
     if any(any(j==1 for j in check[i]) for i in range(N)):
         cnt = -1
     print(cnt)
