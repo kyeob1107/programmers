6221f4be8275548136ce856ab83275ed5b995a7d
곽승엽 <154004299+kyeob1107@users.noreply.github.com>
2024-07-01 11:24:18 +0900

    [Silver II] Title: 한 줄로 서기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
@@ -4,9 +4,8 @@ input = sys.stdin.readline
 N = int(input())
 info = map(int, input().split())
 line = [0] * N
-indexing = list(range(N))
+# indexing = list(range(N))
 for i, p in enumerate(info):
     height = i + 1
-    line[indexing[p]] =  height
-    indexing.remove(indexing[p])
+    line[[i for i, v in enumerate(line) if not v][p]] =  height
 print(*line)
