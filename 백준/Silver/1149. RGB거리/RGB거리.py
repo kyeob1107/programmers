e79402cec1e88531cfef05d3954bfc0d0a3d368e
곽승엽 <154004299+kyeob1107@users.noreply.github.com>
2024-07-02 15:55:06 +0900

    [Silver I] Title: RGB거리, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,13 +1,14 @@
-# 입력버퍼 하나씩 가져와서 이용하고 그 다음 가지고 오고 하는 방식
+# 미리 다 입력 기록해두고 하는 방식
 import sys
 input = sys.stdin.readline
 
 N = int(input())
-pre_house, cal_cost = [0, 0, 0], [0, 0, 0]
-for _ in range(N):
-    input_data = list(map(int, input().split()))
-    cal_cost[0] = min(pre_house[1] + input_data[0], pre_house[2] + input_data[0])
-    cal_cost[1] = min(pre_house[0] + input_data[1], pre_house[2] + input_data[1])
-    cal_cost[2] = min(pre_house[1] + input_data[2], pre_house[0] + input_data[2])
-    pre_house = cal_cost.copy() # 주의! 그냥 같다하면 메모리 주소가 복사됨
-print(min(pre_house))
+info = [list(map(int, input().split())) for _ in range(N)]
+cal_cost = [0, 0, 0]
+for i in range(N - 1):
+    cal_cost[0] = min(info[i][1] + info[i + 1][0], info[i][2] + info[i + 1][0])
+    cal_cost[1] = min(info[i][0] + info[i + 1][1], info[i][2] + info[i + 1][1])
+    cal_cost[2] = min(info[i][1] + info[i + 1][2], info[i][0] + info[i + 1][2])
+    info[i + 1] = cal_cost.copy() # 주의! 그냥 같다하면 메모리 주소가 복사됨
+
+print(min(info[-1]))
