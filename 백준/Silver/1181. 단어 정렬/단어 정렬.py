bfb6000fec1a3d8688590cf3f76785b44147da13
곽승엽 <154004299+kyeob1107@users.noreply.github.com>
2024-07-04 11:49:53 +0900

    [Silver V] Title: 단어 정렬, Time: 84 ms, Memory: 38372 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,4 +1,5 @@
 # 뤼튼에게 개선해달라고 하면서 알게된 것 sorted의 key인자를 활용하면 한번에 여러기준으로 정렬 바로 가능함
+# 추가로 출력방식 변경
 import sys
 input = sys.stdin.readline
 
@@ -12,5 +13,4 @@ for _ in range(N):
 # 데이터를 길이와 사전순으로 정렬
 sorted_data = sorted(data, key=lambda x: (len(x), x))
 
-for word in sorted_data:
-    print(word)
+print('\n'.join(sorted_data))
