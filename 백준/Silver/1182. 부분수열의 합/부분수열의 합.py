3d15d6cb8551abe1513a931eb42fec0f4b31a5cf
곽승엽 <154004299+kyeob1107@users.noreply.github.com>
2024-07-04 21:41:26 +0900

    [Silver II] Title: 부분수열의 합, Time: 292 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -2,13 +2,15 @@ import sys
 import itertools
 
 def get_subsequences(seq):
-    subsequences = []
-    for r in range(len(seq) + 1):
-        subsequences.extend(itertools.combinations(seq, r))
-    return subsequences
+    for r in range(1, len(seq) + 1):
+        yield from itertools.combinations(seq, r)
 
 input = sys.stdin.readline
 
 N, S = map(int, input().split())
 numbers = list(map(int, input().split()))
-print(sum(1 for s in get_subsequences(numbers) if len(s)!=0 and sum(s) == S))
+
+# 부분수열의 합이 S인 경우의 수를 계산
+count = sum(1 for s in get_subsequences(numbers) if sum(s) == S)
+
+print(count)
