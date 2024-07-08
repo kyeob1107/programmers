c340bf9aa379335f6df477481896da6b5a1b10bb
곽승엽 <154004299+kyeob1107@users.noreply.github.com>
2024-07-08 11:58:03 +0900

    [Bronze III] Title: 이진수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3460. 이진수/이진수.py
+++ 백준/Bronze/3460. 이진수/이진수.py
@@ -1,9 +1,19 @@
-# 내장함수 이용
+# 2진수 아예 내장함수 안 쓰고 구현
 import sys
+
+def con_n_not_builtin(n,q): #n진법 변환기 (conversion to base n)
+    rev_base = ''
+
+    while n > 0:
+        n, mod = n//q, n%q
+        rev_base += str(mod)
+
+    return rev_base[::-1]
+
 input = sys.stdin.readline
 T = int(input())
 for _ in range(T):
-    ans = bin(int(input()))
-    for i, s in enumerate(ans[::-1][:-2]):
+    ans = con_n_not_builtin(int(input()), 2)
+    for i, s in enumerate(ans[::-1]):
         if s == '1':
-           print(i, end=' ') 
+           print(i, end=' ')
