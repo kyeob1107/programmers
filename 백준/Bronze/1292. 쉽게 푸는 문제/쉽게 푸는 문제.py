b9b50310a694554cbd2dc2a574ecbbdce993b95f
곽승엽 <154004299+kyeob1107@users.noreply.github.com>
2024-07-09 16:20:54 +0900

    [Bronze I] Title: 쉽게 푸는 문제, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.py
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.py
@@ -1,5 +1,8 @@
-# 수학적으로 바로 계산해보려는 것 일단 정리 바로 안되서 프로토타입
+# 수학적으로 바로 계산해보려는 것, 추가로 정리해서 해보는 것
 import sys
+
+def sum_n(n):
+    return n * (n + 1) //2
 
 def sum_square(n):
     return n * (n + 1) * (2*n + 1) //6
@@ -7,17 +10,14 @@ def sum_square(n):
 input = sys.stdin.readline
 
 A, B = map(int, input().split())
-# 최대 1000까지면 45까지 등장함
-number = [n * (n + 1) //2 for n in range(1, 46)]
-# print(number)
+
 a_check = True
-for num in range(len(number)):
-    if number[num] >= A and a_check:
-        a_n = num + 1
+for n in range(1, 46):
+    if sum_n(n) >= A and a_check:
+        a_n = n
         a_check = False
-    if number[num] >= B:
-        b_n = num + 1
+    if sum_n(n) >= B:
+        b_n = n
         break
-# print(a_n, b_n)
-# print(sum_square(b_n), sum_square(a_n), (number[b_n - 1] - B) * b_n, (number[a_n - 1] - A + 1) * a_n)
-print(sum_square(b_n) - sum_square(a_n) - (number[b_n - 1] - B) * b_n + (number[a_n - 1] - A + 1) * a_n)
+
+print(sum_square(b_n) - sum_square(a_n) - (sum_n(b_n) - B) * b_n + (sum_n(a_n) - A + 1) * a_n)
