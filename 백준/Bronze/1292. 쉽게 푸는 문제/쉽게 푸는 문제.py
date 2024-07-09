1e08cdce7a39047f640315001d48facc1036ff6d
곽승엽 <154004299+kyeob1107@users.noreply.github.com>
2024-07-09 16:14:01 +0900

    [Bronze I] Title: 쉽게 푸는 문제, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.py
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.py
@@ -1,10 +1,23 @@
-# 단순 무식하게
+# 수학적으로 바로 계산해보려는 것 일단 정리 바로 안되서 프로토타입
 import sys
+
+def sum_square(n):
+    return n * (n + 1) * (2*n + 1) //6
+
 input = sys.stdin.readline
 
 A, B = map(int, input().split())
-
-series = []
-for i in range(int((2*B)**0.5 + 1) + 1):
-    series += [i] * i
-print(sum(series[A-1:B]))
+# 최대 1000까지면 45까지 등장함
+number = [n * (n + 1) //2 for n in range(1, 46)]
+# print(number)
+a_check = True
+for num in range(len(number)):
+    if number[num] >= A and a_check:
+        a_n = num + 1
+        a_check = False
+    if number[num] >= B:
+        b_n = num + 1
+        break
+# print(a_n, b_n)
+# print(sum_square(b_n), sum_square(a_n), (number[b_n - 1] - B) * b_n, (number[a_n - 1] - A + 1) * a_n)
+print(sum_square(b_n) - sum_square(a_n) - (number[b_n - 1] - B) * b_n + (number[a_n - 1] - A + 1) * a_n)
