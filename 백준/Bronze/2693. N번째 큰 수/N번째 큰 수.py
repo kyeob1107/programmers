4bbcec5829e10279b37dc3fa68ac633356b272e6
곽승엽 <154004299+kyeob1107@users.noreply.github.com>
2024-07-09 14:56:22 +0900

    [Bronze I] Title: N번째 큰 수, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.py
+++ 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.py
@@ -1,37 +1,13 @@
-# 내장 함수 최대한 안쓰고 해보기 # 모아뒀다가 프린트 따로
+# 제일 단순 무식하게 # 출력 모아뒀다가 한번에
 import sys
 
 def n_largest(array):
-    largest = []
-    for i in array:
-        if len(largest) < 3:
-            if not largest:
-                largest.append(i)
-            else:
-                if i >= largest[0]:
-                    largest.insert(0, i)
-                elif i < largest[-1]:
-                    largest.append(i)
-                else:
-                    largest.insert(1, i)
-                    
-        else:
-            if i >= largest[0]:
-                largest.insert(0, i)
-            elif i >= largest[1]:
-                largest.insert(1, i)
-            elif i >= largest[2]:
-                largest.insert(2, i)
-            
-            if len(largest) > 3:
-                largest.pop()
-    return largest[-1]
+    return sorted(array, reverse= True)[2]
 
 input = sys.stdin.readline
 T = int(input())
 ans = []
 for _ in range(T):
     ans.append(n_largest(list(map(int, input().split()))))
-
 for p in ans:
     print(p)
