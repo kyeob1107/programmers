b40d6b1dab765bd5f8e0d57f64f5aab9a8d514f0
곽승엽 <154004299+kyeob1107@users.noreply.github.com>
2024-07-11 11:50:30 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,27 +1,33 @@
-# 다른 분 것 참고
+# pypy3 1등 코드 살짝 이해 편하게 수정해서 테스트해보는 용
 import sys
 input = sys.stdin.readline
+n = int(input())
+A = list(map(int, input().split()))
+O = list(map(int, input().split()))
 
-def dfs(idx, result, add, sub, mul, div):
-    global max_res, min_res
-    if idx == N:
-        max_res = max(max_res, result)
-        min_res = min(min_res, result)
-        return 
-    if add > 0:
-        dfs(idx + 1, result + series[idx], add - 1, sub, mul, div)
-    if sub > 0:
-        dfs(idx + 1, result - series[idx], add, sub - 1, mul, div)
-    if mul > 0:
-        dfs(idx + 1, result * series[idx], add, sub, mul - 1, div)
-    if div > 0:
-        dfs(idx + 1, int(result / series[idx]), add, sub, mul, div - 1)
-
-N = int(input())
-series = list(map(int, input().split()))
-add, sub, mul, div = map(int, input().split()) # + - * //
-min_res = 10**9
-max_res = -10**9
-dfs(1, series[0], add, sub, mul, div)
-print(max_res)
-print(min_res)
+P = {(0, 0, 0, 0): [A[0], A[0]]}
+for i in range(1, n):
+    P_ = {}
+    f = (
+        lambda k, z: k + z,
+        lambda k, z: k - z,
+        lambda k, z: k * z,
+        lambda k, z: k // z if k > 0 else -(-k // z)
+    )
+    for x in P:
+        for j in range(4):
+            if x[j] < O[j]:
+                _ = list(x)
+                _[j] += 1
+                _ = tuple(_)
+                u = f[j](P[x][0], A[i])
+                v = f[j](P[x][1], A[i])
+                if u < v:
+                    u, v = v, u
+                if _ in P_:
+                    P_[_] = [max(P_[_][0], u), min(P_[_][1], v)]
+                else:
+                    P_[_] = [u, v]
+    P = P_
+for i in P[tuple(O)]:
+    print(i)
