5e77811b7c30d47430a640ccba6c72810a58c44f
곽승엽 <154004299+kyeob1107@users.noreply.github.com>
2024-07-16 17:06:45 +0900

    [Gold IV] Title: 가르침, Time: 1740 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.py
+++ 백준/Gold/1062. 가르침/가르침.py
@@ -1,34 +1,36 @@
-# 레퍼런스 참고해서 작성
-import sys
+# 레퍼런스 원본
 from itertools import combinations
 
-input = sys.stdin.readline
-N, K = map(int, input().split())
-if K < 5:
-    print(0)
-elif K == 26:
-    print(N)
-else:
+def solve():
+    N, K  = map(int, input().split())
+    if K < 5:
+        return 0
+    if K == 26:
+        return N
+
     essential = {'a', 'n', 't', 'i', 'c'}
     words = [input()[4:-4] for _ in range(N)]
+
     all_need = set()
-    for word in words:
-        all_need = all_need | set(word)
-    all_need -= essential
-    
+    for i in range(N):
+        for char in words[i]:
+            if char not in essential:
+                all_need.add(char)
+
     if len(all_need) + 5 <= K:
-        print(N)
-    
-    else:
-        res = 0
-        for comb in combinations(all_need, K-5):
-            learn = set(comb) | essential
-            cnt = N
-            for word in words:
-                for w in word:
-                    if w not in learn:
-                        cnt -= 1
-                        break
-            if cnt > res:
-                res = cnt
-        print(res)
+        return N
+
+    MAX = 0
+    for comb in combinations(all_need, K-5):
+        comb = set(comb) | essential
+        cnt = 0
+        for word in words:
+            for char in word:
+                if char not in comb:         # 변경부분
+                    break                    # 변경부분
+            else:                            # 변경부분
+                cnt += 1                     # 변경부분
+        if cnt > MAX:
+            MAX = cnt
+    return MAX
+print(solve())
