9ca96e534390bde41922aa833c48666d8acb773e
곽승엽 <154004299+kyeob1107@users.noreply.github.com>
2024-07-25 10:07:13 +0900

    [Gold V] Title: 최소비용 구하기, Time: 192 ms, Memory: 38416 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -1,47 +1,41 @@
-# 다익스트라 알고리즘 찾아보고 사용 -> 왜 자꾸 2%대에서 틀리는지 모르겠음
-# 이유 찾음 중복 노선 있을 경우 무조건 나중에 나온 노선의 값으로 계산해서 그럼, 그래서 수정했음2(더 괜찮은 방식 있는 듯해서 수정)
+# 뤼튼이 알려준 다익스트라 알고리즘 우선순위 큐(힙)이용하는 방식 -> 왜 자꾸 2%대에서 틀리는지 모르겠음
+# 이유 찾음 중복 노선 있을 경우 무조건 나중에 나온 노선의 값으로 계산해서 그럼, 
+# 그래프 입력 방식 및 저장 방식 바꿔서
+import heapq
 import sys
 input = sys.stdin.readline
 N = int(input().strip()) # 도시갯수
 M = int(input().strip()) # 버스갯수
 
-graph = [[] for _ in range(N + 1)]
-visited = [0] * (N + 1)
-init = float('inf') # 100_000 * M
-cost = [init] * (N + 1)
-
+graph = [dict() for node in range(N + 1)]
 for _ in range(M):
     a, b, c = map(int, input().split())
-    graph[a].append((b, c))
+    if b in graph[a]:
+        graph[a][b] = min(graph[a][b], c)
+    else:
+        graph[a][b] = c
 
 start, end = map(int, input().split())
 
-def get_smallest_city():
-    min_val = init
-    index = 0
-    for i in range(1, N + 1):
-        if cost[i] < min_val and not visited[i]:
-            min_val = cost[i]
-            index = i
-    return index
+def dijkstra2(graph, start):
+    distances = [float('inf')] * (N + 1)
+    distances[start] = 0
+    queue = [(0, start)]
 
-def dijkstra(start):
-    cost[start] = 0
-    visited[start] = 1
-    for route in graph[start]:
-        if cost[route[0]] > route[1]:
-            cost[route[0]] = route[1]
-    
-    for i in range(N - 1):
-        now = get_smallest_city()
-        visited[now] = 1
-        for bus in graph[now]:
-            cost_cal = cost[now] + bus[1]
-            if cost_cal < cost[bus[0]]:
-                cost[bus[0]] = cost_cal
+    while queue:
+        current_distance, current_node = heapq.heappop(queue)
 
-if start == end:
-    print(0)
-else:
-    dijkstra(start)
-    print(cost[end])
+        if current_distance > distances[current_node]:
+            continue
+
+        for neighbor, weight in graph[current_node].items():
+            distance = current_distance + weight
+
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(queue, (distance, neighbor))
+
+    return distances
+
+
+print(dijkstra2(graph, start)[end])
