ba94b013a9503d9646ae3b825fd260d208c69b21
곽승엽 <154004299+kyeob1107@users.noreply.github.com>
2024-07-25 11:29:42 +0900

    [Gold IV] Title: 최단경로, Time: 584 ms, Memory: 60520 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -0,0 +1,40 @@
+import heapq
+import sys
+input = sys.stdin.readline
+V, E = map(int, input().split()) # 정점, 간선
+start = int(input().strip())
+graph = [{} for _ in range(V + 1)]
+for _ in range(E):
+    u, v, w = map(int, input().split())
+    if v in graph[u]:
+        graph[u][v] = min(graph[u][v], w)
+    else:
+        graph[u][v] = w
+
+init = float('inf')
+
+def dijkstra(graph, start):
+    distances = [init] * (V + 1)
+    distances[start] = 0
+    queue = [(0, start)]
+    
+    while queue:
+        current_distance, current_node = heapq.heappop(queue)
+        if current_distance > distances[current_node]:
+            continue
+        for neighbor, weight in graph[current_node].items():
+            distance = current_distance + weight
+
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(queue, (distance, neighbor))
+    
+    return distances
+
+for i, distance in enumerate(dijkstra(graph, start)):
+    if i == 0:
+        continue
+    if distance == init:
+        print("INF")
+    else:
+        print(distance)
